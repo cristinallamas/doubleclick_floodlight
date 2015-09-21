4e9ab44ac6ddb0ea07244fd7d153de8dc2e7c9bd
Cristina Llamas <tucarapareceunarodilla@gmail.com>
2015-09-21 16:52:16 +0100

    Module files added, first version.

---
+++ theme/doubleclick_floodlight_tag.tpl.php
@@ -0,0 +1,13 @@
+<!--
+Start of DoubleClick Floodlight Tag: Please do not remove
+This tag must be placed between the <body> and </body> tags, as close as possible to the opening tag.
+-->
+<script type="text/javascript">
+var axel = Math.random() + "";
+var a = axel * 10000000000000;
+document.write('<iframe src="https://<?php print $account_id?>.fls.doubleclick.net/activityi;src=<?php print $account_id?>;type=<?php print $type?>;cat=<?php print $cat?>;ord=1;num=' + a + '?" width="1" height="1" frameborder="0" style="display:none"></iframe>');
+</script>
+<noscript>
+<iframe src="https://<?php print $account_id?>.fls.doubleclick.net/activityi;src=<?php print $account_id?>;type=<?php print $type?>;cat=<?php print $cat?>;ord=1;num=1?" width="1" height="1" frameborder="0" style="display:none"></iframe>
+</noscript>
+<!-- End of DoubleClick Floodlight Tag: Please do not remove -->
